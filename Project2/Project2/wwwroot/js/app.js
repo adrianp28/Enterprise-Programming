b2aa2f2ae6bce093c247279ce07514725a4a8014
adrianp28 <tstewart11@uco.edu>
2018-02-12 00:22:06 -0600

    Adding Project 2

---
+++ Project2/Project2/wwwroot/js/app.js
@@ -0,0 +1,7 @@
+﻿import '../dist/HarryStyles.css';
+import 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social';
+import 'bootstrap-social/bootstrap-social.css'
+import 'font-awesome-webpack';
