643455ae01cde43e9bb1c986df0b1c2b768ae417
Tyler Stewart <35243663+adrianp28@users.noreply.github.com>
2018-06-05 17:33:15 -0500

    Update search.service.ts

--- Project3/Project3/ClientApp/app/components/Search/search.service.ts
+++ Project3/Project3/ClientApp/app/components/Search/search.service.ts
@@ -10,18 +10,18 @@ export class SearchService {
     constructor(private http: Http) { }
 
     getReps(address: string) {
-        return this.http.get('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDsbN4O4BdkYBlMAqZmBGpBqcoiQB2_fm4&address=' + address)
+        return this.http.get('https://www.googleapis.com/civicinfo/v2/representatives?key=&address=' + address)
             .map(response => response.json());
 
     }
     getElections(address: string) {
-        return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDsbN4O4BdkYBlMAqZmBGpBqcoiQB2_fm4&address=' + address)
+        return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?key=&address=' + address)
             .map(response => response.json());
     }
     getElection(address: string, electionID: string) {
-        return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=' + address + '&electionId=' + electionID + '&key=AIzaSyDsbN4O4BdkYBlMAqZmBGpBqcoiQB2_fm4')
+        return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=' + address + '&electionId=' + electionID + '&key=')
             .map(response => response.json());
-        //return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=904%20Chartrand%20Ct%20Edmond%20OK%2073034&electionId=2000&key=AIzaSyDsbN4O4BdkYBlMAqZmBGpBqcoiQB2_fm4')
+        //return this.http.get('https://www.googleapis.com/civicinfo/v2/voterinfo?address=904%20Chartrand%20Ct%20Edmond%20OK%2073034&electionId=2000&key=')
         //    .map(response => response.json());
         
     }
